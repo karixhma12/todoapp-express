c377b874d6726fbec526fda6b67f784a96e85954
Karishma <karishma.7022@gmail.com>
2026-05-26 16:46:50 +0530

    feat: signup endpoint

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(express.json());
 
 let users = [];
 
+app.post("/signup",(req,res)=>{
+    let username = req.body.username;
+    let password = req.body.password;
+    users.push({username,password, todos:[]});
+    res.json({message : "You have signed up!"});
+})
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
